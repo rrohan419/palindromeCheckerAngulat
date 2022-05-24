0e3c73bb150bc17cbdaf960ef8b40eb7f681fedc
Rohan <rrohan419@gmail.com>
2022-05-24 19:27:55 +0530

    palindrome checker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'palindromeCheck';
+  number=0;
+  tempVar = 0;
+
+  checkIfPalindrone(number:number)
+  {
+    var tempStorage= number;
+    console.log(tempStorage,"good evening");
+    var remainder =0;
+
+    while(tempStorage!=0)
+    {
+      
+      remainder = tempStorage%10;
+      tempStorage= Math.floor(tempStorage/10);
+      // console.log(tempStorage);
+    
+      this.tempVar = ((this.tempVar*10)+remainder);
+    }
+    console.log(this.tempVar);
+    console.log(number);
+    if(number==this.tempVar)
+    {
+      this.tempVar=0;
+      alert("it is a palindrome");
+    }
+    else
+    {
+      this.tempVar=0;
+      alert("it is not a palindrome");
+    }
+  }
 }
